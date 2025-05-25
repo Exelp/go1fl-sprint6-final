2e24b48ad3a5b0792a4df616f5f4aeb35f009196
Yarutin Maxim <www2x2@mail.ru>
2025-05-26 00:57:09 +0300

    First commit

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,31 @@
 package server
+
+import (
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/handlers"
+	"log"
+	"net/http"
+	"time"
+)
+
+type Server struct {
+	Logger *log.Logger
+	Http   *http.Server
+}
+
+func NewServer(loger *log.Logger) *Server {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handlers.ReturnIndexHandler)
+	mux.HandleFunc("/upload", handlers.UploadHandler)
+	httpServer := &http.Server{
+		Addr:         ":8080",
+		Handler:      mux,
+		ErrorLog:     loger,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  15 * time.Second,
+	}
+	return &Server{
+		Logger: loger,
+		Http:   httpServer,
+	}
+}
